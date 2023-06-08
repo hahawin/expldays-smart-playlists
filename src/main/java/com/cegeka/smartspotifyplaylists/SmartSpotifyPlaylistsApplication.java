90906ddf1824df6a880225dbf3e0299b491d18e6
jasperh <jasper.hawinkel@gmail.com>
2023-06-08 18:06:33 +0200

    Init project

---
+++ src/main/java/com/cegeka/smartspotifyplaylists/SmartSpotifyPlaylistsApplication.java
@@ -0,0 +1,13 @@
+package com.cegeka.smartspotifyplaylists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSpotifyPlaylistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSpotifyPlaylistsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cegeka/smartspotifyplaylists/SmartSpotifyPlaylistsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cegeka.smartspotifyplaylists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSpotifyPlaylistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
